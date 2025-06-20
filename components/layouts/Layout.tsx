515c919c097686897a3ea34bd2cdaddeb8e4c8eb
trizahc <trizahchepkirui07@gmail.comm>
2025-06-20 18:49:15 -0500

    Set up Header, Footer, Layout and landing page

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,15 @@
-const Layout: React.FC = () => {
-  return <div>Layout Component</div>;
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
